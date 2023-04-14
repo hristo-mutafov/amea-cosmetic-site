469a8d7b66a7bdcaa209e3b10626015678fc94a4
Ici-pici <hristomutafov@gmail.com>
2023-04-14 15:54:36 +0300

    Added Trainings section with sliders

--- the_scripts/for_me_page_slider_config.js
+++ the_scripts/for_me_page_slider_config.js
@@ -1,14 +1,51 @@
-const swiper1 = new Swiper('.outer', {
+const swiper1 = new Swiper('.for_me_swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
 
     // Navigation arrows
     navigation: {
-        nextEl: '#next',
-        prevEl: '#prev',
+        nextEl: '#for_me_swiper_next',
+        prevEl: '#for_me_swiper_prev',
     },
 });
+
+const swiper2 = new Swiper('.procedures_swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '#procedures_swiper_next',
+        prevEl: '#procedures_swiper_prev',
+    },
+});
+
+const swiper3 = new Swiper('.trainings_wrapper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '#trainings_wrapper_next',
+        prevEl: '#trainings_wrapper_prev',
+    },
+});
+
+const swiper4 = new Swiper('.training_wrapper2', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '#training_wrapper2_next',
+        prevEl: '#training_wrapper2_prev',
+    },
+});
+
 
 const swiper = new Swiper('.nested', {
     // Optional parameters
@@ -23,7 +60,7 @@ const swiper = new Swiper('.nested', {
 });
 
 delay = 10000;
-next_buttons = Array.from(document.querySelectorAll('#next'));
+next_buttons = Array.from(document.querySelectorAll('.swiper-button-next'));
 
 setInterval(() => {
     for (const button of next_buttons) {
