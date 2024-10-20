eaed02db808a54f9d8920356a7daf5029774a279
hristo-mutafov <hristomutafov@gmail.com>
2024-10-20 11:38:36 +0300

    Logo btn is no longer triggering mobile nav.

--- the_scripts/mobileMenu.js
+++ the_scripts/mobileMenu.js
@@ -5,9 +5,14 @@
 const mobileMenu = document.querySelector('.header .mobile_nav #mobile-menu');
 const navWrapper = document.getElementById('nav_wrapper');
 const nav = document.getElementById('navigation');
+const logoBtn = document.getElementById('logo_btn');
 
 mobileMenu.addEventListener('click', (e) => {
     mobileMenu.classList.toggle('is-active');
     nav.classList.toggle('active');
     navWrapper.classList.toggle('active');
+});
+
+logoBtn.addEventListener('click', (e) => {
+    e.stopPropagation();
 });
