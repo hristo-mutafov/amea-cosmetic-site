34d625061deb5f3abbfc65f670067d71c954132d
hristo-mutafov <hristomutafov@gmail.com>
2024-10-20 11:46:25 +0300

    Removed redundant navigation script and replace it with html a tag.

--- the_scripts/achievements/navigation.js
+++
@@ -1,7 +0,0 @@
-const seeMoreButton = document.querySelector('.opinions .see-more');
-
-if (seeMoreButton) {
-    seeMoreButton.addEventListener('click', function () {
-        window.location.href = '/achievements';
-    });
-}
